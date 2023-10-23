a31f2c20011a999be6cb388ddec6b68dc5c091b3
hibiki <k022g1084@g.neec.ac.jp>
2023-10-23 12:09:56 +0900

    スクロールの設定

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <Novice.h>
+#include <math.h>
 
 const char kWindowTitle[] = "GC1D_03_イトウヒビキ_タイトル";
 
@@ -11,6 +12,47 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+	const int World_Width = 1280 * 83;
+
+	//[42]で約１分　 2分=[84] ３分＝[126]
+	int backgroundX[84]{
+	    0,         1280 * 1,  1280 * 2,  1280 * 3,  
+		1280 * 4,  1280 * 5,  1280 * 6,  1280 * 7,
+	    1280 * 8,  1280 * 9,  1280 * 10, 1280 * 11,
+		1280 * 12, 1280 * 13, 1280 * 14, 1280 * 15,
+	    1280 * 16, 1280 * 17, 1280 * 18, 1280 * 19, 
+		1280 * 20, 1280 * 21, 1280 * 22, 1280 * 23,
+	    1280 * 24, 1280 * 25, 1280 * 26, 1280 * 27,
+		1280 * 28, 1280 * 29, 1280 * 30, 1280 * 31,
+	    1280 * 32, 1280 * 33, 1280 * 34, 1280 * 35,
+		1280 * 36, 1280 * 37, 1280 * 38, 1280 * 39,
+	    1280 * 40, 1280 * 41, 1280 * 42, 1280 * 43,
+		1280 * 44, 1280 * 45, 1280 * 46, 1280 * 47,
+	    1280 * 48, 1280 * 49, 1280 * 50, 1280 * 51,
+		1280 * 52, 1280 * 53, 1280 * 54, 1280 * 55,
+	    1280 * 56, 1280 * 57, 1280 * 58, 1280 * 59,
+		1280 * 60, 1280 * 61, 1280 * 62, 1280 * 63,
+	    1280 * 64, 1280 * 65, 1280 * 66, 1280 * 67,
+		1280 * 68, 1280 * 69, 1280 * 70, 1280 * 71,
+	    1280 * 72, 1280 * 73, 1280 * 74, 1280 * 75,
+		1280 * 76, 1280 * 77, 1280 * 78, 1280 * 79,
+	    1280 * 80, 1280 * 81, 1280 * 82, 1280 * 83,
+	};
+
+	for (int i = 0; i < 84; i++) {
+		Novice::LoadTexture("./Game_screen2.png");
+	};
+
+	int posX = 40;
+	int posY = 560;
+	int posrad = 1;
+	int posW = 50;
+	int posH = 50;
+	int speed = 15;
+	int ScrollX = posX + posrad;
+	int playerScroll = posX - ScrollX;
+	int backgroundHandle = Novice::LoadTexture("./Game_screen2.png");
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -25,6 +67,32 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		posX += speed;
+
+		ScrollX = posX + posrad;
+
+		if (ScrollX < 0) {
+			ScrollX = 0;
+		}
+		if (ScrollX > World_Width - 1280) {
+			ScrollX = World_Width - 1280;
+		}
+		playerScroll = posX - ScrollX;
+
+		if (posX >= backgroundX[21]) {
+			speed = 20;
+		}
+		if (posX >= backgroundX[42]) {
+			speed = 25;
+		}
+		if (posX >= backgroundX[63]) {
+			speed = 30;
+		}
+
+		if (posX >= 1280 * 83) {
+			speed = 0;
+		}
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -32,6 +100,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		Novice::ScreenPrintf(0, 0, "%d", posX);
+		Novice::ScreenPrintf(0, 20, "%d", speed);
+		for (int i = 0; i < 84; i++) {
+			Novice::DrawSprite(backgroundX[i] - ScrollX, 0, backgroundHandle, 1, 1, 0.0f, WHITE);
+		}
+		Novice::DrawBox(
+		    playerScroll - posrad, posY - posrad, posW, posH, 0.0, WHITE, kFillModeSolid);
 
 		///
 		/// ↑描画処理ここまで
